b838d44a2bf036bce4a56bf801e8aee1495ba8fb
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-22 15:59:16 +0530

    cookies and try catch

--- routes/review.js
+++ routes/review.js
@@ -3,20 +3,30 @@ const Product = require('../models/product');
 const Review = require('../models/review');
 const router = express.Router();
 
-
 router.post('/products/:productId/review' , async(req,res)=>{
-   let {productId} = req.params;
-   let {rating, comment} = req.body;
-   const product = await Product.findById(productId);
-   const review = new Review({rating, comment});
-   console.log(review);
-   console.log(product);
-   product.reviews.push(review);
-   await review.save();
-   await product.save();
 
-   res.redirect(`/products/${productId}`);
+    try{
+            // console.log(req.body);
+        let {productId} = req.params;
+        let {rating, comment} = req.body;
+        const review = new  Review({rating, comment});
+        const product = await Product.findById(productId);
+        product.reviews.push(review);
+
+        console.log(product);
+        console.log(review);
+        
+        await product.save()
+        await review.save()
+
+        res.redirect(`/products/${productId}`);
+    }
+    catch(e){
+        res.render('error' , {err: e.message} )
+    }
+    
 })
+
 
 
 module.exports = router;
