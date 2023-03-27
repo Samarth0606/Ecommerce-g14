b3f0b71f3444a1b1ebb966d3d56c986590673ec8
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-27 13:46:05 +0530

    flash and session

--- app.js
+++ app.js
@@ -2,41 +2,54 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const seedDB = require('./seed');
-const productRoutes = require("./routes/productRoutes");
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const reviewRoutes = require("./routes/review");
+const session = require('express-session');
+const flash = require('connect-flash');
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost:27017/shopping-sam-app')
+    .then(() => console.log('DB Connected'))
+    .catch((err) => console.log(err));
+
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+const sessionConfig = {
+    secret: 'weneedsomebettersecret',
+    resave: false,
+    saveUninitialized: true
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 
 
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/shopping-g14-app')
-.then(()=>{console.log("DB connected")})
-.catch((err)=>{console.log(err)})
- 
-
-app.engine('ejs' , ejsMate);
-app.set('view engine' , 'ejs');
-app.set('views' , path.join(__dirname,'views'));
-// now for public folder
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({extended:true}));
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+// Routes
+const productRoutes = require('./routes/product');
+const reviewRoutes = require('./routes/review');
 
 
-// seeding dummy data
-// seedDB();
 
-// routes
-app.use(productRoutes); 
+app.use(productRoutes);
 app.use(reviewRoutes);
 
 
 const port = 5000;
-app.listen(port,()=>{
-    console.log(`server connected at port : ${port} `);
-})
 
-
+app.listen(port, () => {
+    console.log(`server running at port ${port}`);
+});
